2aae10bfa2ab5854be3859e4055928456c2bc048
mabrotrix <mabrotrix@gmail.com>
2020-03-04 15:47:06 +0530

    Updates

---
+++ precache-manifest.c3a3fc4f8b059200794d0064b48fd1c3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f192147cf2413addc2c74d45e6fd83f",
+    "url": "/kimbo/index.html"
+  },
+  {
+    "revision": "c162a8d0720f19a0e327",
+    "url": "/kimbo/static/css/main.071f0ae7.chunk.css"
+  },
+  {
+    "revision": "89d9fea0ff81318873d3",
+    "url": "/kimbo/static/js/2.a5e8d4f6.chunk.js"
+  },
+  {
+    "revision": "349a03097e8eddedac8cc67f26a159d7",
+    "url": "/kimbo/static/js/2.a5e8d4f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c162a8d0720f19a0e327",
+    "url": "/kimbo/static/js/main.e676a583.chunk.js"
+  },
+  {
+    "revision": "a1e60ed94bfbbb047213",
+    "url": "/kimbo/static/js/runtime-main.c2dbf46a.js"
+  },
+  {
+    "revision": "2cc2525d3edd235681727333db49d5ee",
+    "url": "/kimbo/static/media/login-indesign.2cc2525d.png"
+  },
+  {
+    "revision": "c55819c4dba728837f134d044e01c6ea",
+    "url": "/kimbo/static/media/profile.c55819c4.png"
+  }
+]);
